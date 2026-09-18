570a03af87a4dcced7b536ce2dc627ddd00d221e
David Grethlein <12dgrethlein@gmail.com>
2026-09-18 08:34:42 -0400

    moving time series forecasting modules into sub-package, building out top-level main script for running experiments using command line arguments

--- src/__main__.py
+++ src/__main__.py
@@ -19,6 +19,8 @@ Package Contents
 import traceback
 from typing import Dict
 
+from .pre_process import load_dataset_df_into_series_dfs
+
 from .utils.args import add_ts_cluster_args_parser
 from .utils.args import add_ts_forecast_args_parser
 from .utils.args import get_args_parser
@@ -27,13 +29,16 @@ from .utils.args import parse_args
 from .utils.misc import dbg, err
 
 
-
-
+#==============================================================================
+#       TOP-LEVEL MAIN PACKAGE SCRIPT FOR INVOKATION FUNCTION(s)
+#==============================================================================
 def get_parsed_args_as_dict() -> Dict:
-    """Summary
+    """Gets a dictionary containing the parsed command line arguments for running
+    experiments on time series data in a local (or remote) computing environment.
 
     Returns:
-        Dict: Description
+        Dict: A dictionary containing command line arguments parsed by
+            an :class:`argparse.ArgumentParser`.
     """
     pargs = None
 
@@ -50,6 +55,63 @@ def get_parsed_args_as_dict() -> Dict:
     return pargs
 
 
+def main():
+    """Main function for running experiments using this project's (./src/) top-level package.
+    """
+    try:
+        # Parse command line arguments.
+        main_args = get_parsed_args_as_dict()
+
+        # Run time series clustering experiments.
+        if main_args["command"] == "cluster":
+            ts_cluster(main_args)
+
+        # Run time series forecasting experiments.
+        elif main_args["command"] == "forecast":
+            ts_forecast(main_args)
+
+        # Run time series prototyping experiments.
+        elif main_args["command"] == "prototype":
+            ts_prototype(main_args)
+
+    except (AttributeError, IndexError, KeyError, TypeError, ValueError):
+        print(f"\n// {err()}  Couldn't run main package script invokation function!\n")
+        traceback.print_exc()
+
+
+#==============================================================================
+#       COMMAND-SPECIFIC TOP-LEVEL MAIN PACKAGE FUNCTION(s)
+#==============================================================================
+def ts_cluster(args_dict : Dict):
+    """Top-level function for running all time series clustering experiments.
+
+    Args:
+        args_dict (Dict): A dictionary containing command line arguments parsed by
+            an :class:`argparse.ArgumentParser`.
+    """
+    dfs, names = load_dataset_df_into_series_dfs()
+
+
+def ts_forecast(args_dict : Dict):
+    """Top-level function for running all time series forecasting experiments.
+
+    Args:
+        args_dict (Dict): A dictionary containing command line arguments parsed by
+            an :class:`argparse.ArgumentParser`.
+    """
+    dfs, names = load_dataset_df_into_series_dfs()
+
+
+def ts_prototype(args_dict : Dict):
+    """Top-level function for running all time series prototyping experiments.
+
+    Args:
+        args_dict (Dict): A dictionary containing command line arguments parsed by
+            an :class:`argparse.ArgumentParser`.
+    """
+    dfs, names = load_dataset_df_into_series_dfs()
+
+
 #==============================================================================
 #       SCRIPT ENTRY POINT
 #==============================================================================
@@ -57,8 +119,7 @@ if __name__ == "__main__":
 
     print(f"\n// {dbg()}  Running File['{__file__}'] as __main__!\n")
 
-
-    get_parsed_args_as_dict()
-
+    # Top-level function for running experiments.
+    main()
 
     print(f"\n// {dbg()}  All done here, nothing to see!\n")
